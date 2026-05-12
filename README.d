f1788e60d4b5d200929bfa0e7787fc98e768e10e
Prachi Kumari <prachi16dec@gmail.com>
2026-05-12 23:43:04 +0530

    initial commit

---
+++ README.d
@@ -0,0 +1,22 @@
+# Video Meeting
+
+Video Meeting is a web-based video conferencing application that allows users to create and join meetings using a unique meeting code.
+
+## Features
+
+- User Authentication
+- Create & Join Meetings
+- Real-time Video Calling
+- Meeting History
+- Responsive UI
+
+## Tech Stack
+
+- React.js
+- Node.js
+- Express.js
+- MongoDB
+- Socket.io
+- WebRTC
+- Material UI
+
